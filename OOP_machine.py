e0ffec36a1b86baae7b09a340c31db3a02d5e853
twan3617 <tonywang205@yahoo.com.au>
2021-09-05 15:22:19 +1000

    Finished coding OOP_machine

--- OOP_machine.py
+++ OOP_machine.py
@@ -13,25 +13,35 @@ import numpy as np
 ### Use OOP to remove redundancies
 ### Also, use OOP to create a state-wide and nation-wide list of vending machines.
 
+class ShopItem():
+    def __init__(self, cost, stock, sales):
+        self.cost = cost
+        self.stock = stock
+        self.sales = sales
+
+### Coins are also a shop item; the "sales" attribute refers to the number of # coins used.
+
 class VendingMachine():
     def __init__(self, money):
-        self.items = ["Water", "Coke", "Diet Coke", "Iced Tea", "Swiss Chocolate", "Candy", "Chips",
-        "Bubble Gum", "Turkish Delight"]
-        self.costs = [0.75, 1.20, 1.20, 1.00, 1.50, 0.95, 1.10, 0.50, 1.20] ### A dictionary here would help!
-
-        self.num_items = len(self.items)
 
         self.money = money #money put into the vending machine
-        self.coins = [1, 0.50, 0.20, 0.10, 0.05] #Coin denominations
 
+        self._max_items = 10 #Maximum stock of items and coins
+        self._max_coins = 20
         self._PIN = 0000 #Secret maintenance PIN
         self._balance = money #Current remaining balance
-        self._max_items = 10
-        self._max_coins = 20
 
-        self._stock = self._max_items * np.ones(self.num_items)
-        self._num_coins = self._max_coins * np.ones(len(self.costs))
-        self._sales = np.zeros(self.num_items)
+        item_names = ["Water", "Coke", "Diet Coke", "Iced Tea", "Swiss Chocolate", "Candy", "Chips",
+        "Bubble Gum", "Turkish Delight"] #List of item names
+        costs = [0.75, 1.20, 1.20, 1.00, 1.50, 0.95, 1.10, 0.50, 1.20]
+        coins = [1, 0.50, 0.20, 0.10, 0.05] #Coin denominations
+        self.num_items = len(item_names)
+        dict_items = dict(zip(item_names, costs))
+
+        self.items = {name: ShopItem(cost, self._max_items, 0) for name, cost in dict_items.items()}
+        self.coins = {denom: ShopItem(denom, self._max_coins, 0) for denom in coins}
+        self._sales = dict(zip(item_names,np.zeros(self.num_items)))
+
         self._income = 0
 
     def DisplayErrorMessage(self):
@@ -39,112 +49,110 @@ class VendingMachine():
         return 
 
     def RestockVend(self):
-        self._stock = self._max_items * np.ones(self.num_items) #restock coins and stock
-        self._num_coins = self._max_coins * np.ones(len(self.costs))
+        for item in self.items.values(): 
+            item.stock = self._max_items
+        for coin in self.coins.values(): 
+            coin.stock = self._max_coins
         print("Restocking performed.")
         return 
 
+    def ComputeCost(self, choice): #Given a list of selections, return the total cost.
+        cost = 0
+        for select in choice: 
+            item_key = list(self.items.keys())[select-1]
+            cost += self.items[item_key].cost
+        return cost
+
     def ShowMenu(self):
-        print(f"Menu:")
-        for i in range(0,len(self.items)):
-            print(f"{i+1}: {self.items[i]}          ${self.costs[i]}") #For future: align the print statements?
-        return 
+        for name, item in self.items.items():
+            print(f"{name}: ${item.cost}")
+        pass
 
     def MakeSelection(self):
         choice = []
+        choice_names = []
         while True: 
-            selection = int(input("Choose an option from 1 to 9, or 10 to quit."))
-            
+            selection = int(input("Choose an option from 1 to 9, or 10 to quit. "))
             ### Check if secret code was given first
             if selection == 99: 
                 #Enter maintenance routine
                 PIN = int(input("Enter PIN: "))
-                if PIN == 0000:
-                    print(f"Items: {self.items} \n Balance: ${self._balance} \n Stock: {self._stock} \n Coins: {self._num_coins} \n Sales: {self._sales} \n Income: ${self._income:.2f}")
+
+                if PIN == 0000: ###Odd thing is happening with \, \n and spacing - fix?
+                    print(f"Balance: ${self._balance}\n\
+                    Income: ${self._income:.2f}\n\
+                    Stock (Items): { {str(item_name): item.stock for item_name, item in self.items.items() } }\n\
+                    Stock (Coins): { {denom: coin.stock for denom, coin in self.coins.items()} }")
                     self.RestockVend()
                     return [], 0
 
             if selection == 10:
-                print("Quitting...")
-                return [10], 0
+                print("Your selections have been made.")
+                return choice, self.ComputeCost(choice)
 
-            ### Otherwise, check whether out of stock or not
-            if self._stock[selection-1] == 0: 
+            item_key = list(self.items.keys())[selection-1]
+            choice_names.append(item_key)
+
+            if self.items[item_key].stock == 0: 
                 self.DisplayErrorMessage() 
+
             else:  
                 choice.append(selection)
 
-                print(f"You have chosen {selection}: {self.items[selection - 1]}, costing ${self.costs[selection - 1]:.2f}")
+                print(f"You have chosen {selection}: {item_key}, costing ${self.items[item_key].cost:.2f}")
                 cont = input("Continue? (y/n) ")
 
                 if cont == "n":  #Use switch() statements here instead?
                     cost = self.ComputeCost(choice)
                     if cost > self.money: 
                         print("You don't have enough money! Returning...")
-                        return 
+                        return [], 0
                     print(f"Your total cost is ${cost:.2f}.")
-                    return (choice, cost)
+                    return choice, cost
 
                 elif cont == "y":
-                    print(f"Your current choices are:")
-                    for choices in choice:
-                        print(f"{self.items[choices-1]}")
-                    pass 
+                    print(f"The running total is ${self.ComputeCost(choice):.2f}, with selections:")
+                    print(choice_names)
                 else: 
                     print("Invalid input.")
-                    return 
-
-    def ComputeCost(self, choice): #given a selection of items, return total cost
-        cost = 0
-        for selection in choice:
-            cost += self.costs[selection-1] 
-        return cost
-
-    def ReturnChange(self, choice, cost): #takes in the selected choices as a list.
-        ### For maintenance routine: if inputs are default values, return.
+                    return choice, self.ComputeCost(choice)          
+        return 
+        
+    def ReturnChange(self, choice, cost): #Takes in selected choices as a list
         if (choice == []) and (cost == 0):
             return
-        change = []
+        change_dict = dict()
         temp_money = cost 
-
-        for index, coins in enumerate(self.coins):
-
+        for denom, coin in self.coins.items():
             temp_money = np.round_(temp_money, 2)
-
-            (mod, remain) = divmod(temp_money, coins) ### Roundoff errors sometimes??
-            ### Evil fix 
-            pocket = min(mod, self._num_coins[index])
-            temp_money = temp_money - pocket * self.coins[index]
-            change.append(pocket)
+            (mod, remain) = divmod(temp_money, denom) ### Roundoff errors sometimes??
+            pocket = min(mod, coin.stock) ### The pocket change taken out of the machine
+            temp_money = temp_money - pocket * coin.cost
+            change_dict[denom] = pocket
             ### Decrease num_coins by pocket
-            self._num_coins[index] = self._num_coins[index] - pocket
+            coin.stock -= pocket
+            coin.sales += pocket
 
         if temp_money != 0: #After spitting out all change, there is still money left.
             print(temp_money)
             print("We don't have enough change for you. Returning your money...")
             return self.money
-        else: 
+        else:
+            self._balance -= cost #Purchase complete: update balance and cost.
+            self._income += cost
             print(f"Thank you for your purchase! Your change is:")
-            for iters, changes in enumerate(change):
-                print(f"{abs(changes):.0f} x {self.coins[iters]:.2f} coin(s)")
+            for denom, pocket in change_dict.items():
+                print(f"{pocket:.0f} x {denom:.2f} coin(s)")
 
-            # Decrease stock and increase sales, increase income. Change baalnce
-        for selections in choice:
-            self._stock[selections-1] -= 1 #note that the stock > 0 check is made at selection.
-            self._sales[selections-1] += 1
-            self._income += self.costs[selections-1]
-            self._balance -= self.costs[selections-1]
-        return 0   
         
+        pass
 
 def main():
     if __name__ == "__main__":
         VM = VendingMachine(10)
-        VM.ShowMenu()
         while True: 
-            (choices, cost) = VM.MakeSelection()
-            if 10 in choices: 
-                break
-            VM.ReturnChange(choices, cost)
-
+            VM.ShowMenu()
+            choice, cost = VM.MakeSelection()
+            VM.ReturnChange(choice, cost)
+    return 0 
 main()
