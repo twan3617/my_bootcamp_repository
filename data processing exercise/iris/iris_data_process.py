77ac23d28154be4cfd825e21a40de7bff04275d0
twan3617 <tonywang205@yahoo.com.au>
2021-09-08 22:43:06 +1000

    data processing for abalone dataset

--- data processing exercise/iris/iris_data_process.py
+++ data processing exercise/iris/iris_data_process.py
@@ -5,11 +5,13 @@ def process():
     with open('iris.data', 'r') as iris:
         irisdata = iris.read()
 
-        # Replace the target string
-        irisdata = irisdata.replace('Iris-setosa', '0')
-        irisdata = irisdata.replace('Iris-versicolor', '1')
-        irisdata = irisdata.replace('Iris-virginica', '2')
+        # Replace the target string with a one-hot encoding
+        irisdata = irisdata.replace('Iris-setosa', '001')
+        irisdata = irisdata.replace('Iris-versicolor', '010')
+        irisdata = irisdata.replace('Iris-virginica', '100')
 
         # Write the file out again
         with open('iris_clean.csv', 'w') as file: ### w will create new file if it doesn't already exist 
             file.write(irisdata)
+
+process()
